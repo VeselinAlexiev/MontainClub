eaf9c29bd3074626614b3f76bbeb2062e2083e51
Ivaylo <ivaylo06@abv.bg>
2024-11-07 12:28:47 +0200

    About page

--- solution.js
+++ solution.js
@@ -1,1 +1,7 @@
-
+document.getElementById('about').addEventListener('click', (e) => {
+    e.preventDefault();
+    document.querySelectorAll('.page').forEach(x => x.classList.add('hide'));
+    document.querySelectorAll('.nav ul li a').forEach(x => x.classList.remove('new-color'));
+    e.target.classList.add('new-color');
+    document.querySelector('.about-page').classList.remove('hide');
+});
